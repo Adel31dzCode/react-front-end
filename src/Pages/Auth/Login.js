e87dee893e24d82e08c0f353cbb7a0bc01ba32af
Adel31_dz <132948186+Adel31dzCode@users.noreply.github.com>
2025-06-04 15:09:21 +0100

    Update Login.js

--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -9,6 +9,7 @@ import '../../Style/Login.css';
 
 
 export default function Login() {
+  console.log(API);
   const [form, setForm] = useState({ email: '', password: '' });
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [InvalidMatch, SetInvalidMatch] = useState(false);
