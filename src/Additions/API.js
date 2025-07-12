c74d8375b0a9d5782745f475236785667a5ac735
Adel31_dz <132948186+Adel31dzCode@users.noreply.github.com>
2025-07-12 01:31:49 +0100

    Update API.js

--- src/Additions/API.js
+++ src/Additions/API.js
@@ -1,4 +1,4 @@
-export const API = "https://laravel-back-end-production.up.railway.app/api/";
+export const API = "https://laravel-back-end-yn5i.onrender.com/api/";
 
 export const RegisterApi = "user/register";
 export const LoginApi = "user/login";
