1c752cd7b9bdfc24ba748cd1e5f7f19e81bb64b7
Adel31_dz <132948186+Adel31dzCode@users.noreply.github.com>
2025-07-12 01:39:13 +0100

    Update API.js

--- src/Additions/API.js
+++ src/Additions/API.js
@@ -1,4 +1,4 @@
-export const API = "https://laravel-back-end-yn5i.onrender.com/api/"; // api
+export const API = "https://laravel-back-end-yn5i.onrender.com/api/";
 
 export const RegisterApi = "user/register";
 export const LoginApi = "user/login";
